3fee3c49b6f490dec84c7d70edfe15967abc7521
swhyeon98 <smreo75@gmail.com>
2023-12-06 18:27:28 +0900

    feat(LottoResult): 로또 결과를 계산하는 기능 추가 - 당첨 번호와 구매 번호 비교 - 당첨 등급 - 수익률 계산

---
+++ src/main/java/lotto/model/LottoResult.java
@@ -0,0 +1,39 @@
+package lotto.model;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LottoResult {
+
+    public Map<Rank, Long> calculateResults(Lotto winningLotto, List<Lotto> purchasedLottos, int bonusNumber) {
+        Map<Rank, Long> results = new HashMap<>();
+        for (Lotto purchasedLotto : purchasedLottos) {
+            Rank rank = getRank(winningLotto, purchasedLotto, bonusNumber);
+            if (rank != Rank.NO_LUCK) {
+                results.put(rank, results.getOrDefault(rank, 0L) + 1);
+            }
+        }
+        return results;
+    }
+
+    private Rank getRank(Lotto winningLotto, Lotto purchasedLotto, int bonusNumber) {
+        boolean isBonusMatched = purchasedLotto.hasBonusNumber(bonusNumber);
+        int matchCount = winningLotto.getMatchCount(purchasedLotto);
+        return Rank.checkWinningStatus(matchCount, isBonusMatched);
+    }
+
+    public double calculateProfit(List<Lotto> purchasedLottos, Map<Rank, Long> results) {
+        long purchaseAmount = purchasedLottos.size() * 1000;
+        long totalPrize = calculateTotalPrize(results);
+        return ((double) totalPrize / purchaseAmount) * 100;
+    }
+
+    private long calculateTotalPrize(Map<Rank, Long> results) {
+        long totalPrize = 0;
+        for (Map.Entry<Rank, Long> entry : results.entrySet()) {
+            totalPrize += entry.getValue() * entry.getKey().getPrizeMoney();
+        }
+        return totalPrize;
+    }
+}
