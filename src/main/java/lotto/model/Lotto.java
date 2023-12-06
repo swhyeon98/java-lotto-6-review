3ce788ee2ff0bdf67f94a65f506974eff5aa1cf9
swhyeon98 <smreo75@gmail.com>
2023-12-06 14:54:33 +0900

    feat(Lotto): 숫자 범위 검증 기능 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -11,10 +11,19 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateNumberSize(numbers);
+        validateNumberInRange(numbers);
+    }
+
+    private void validateNumberSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 입력한 숫자의 개수가 올바르지 않습니다. 6개의 숫자를 입력해야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateNumberInRange(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(number -> number < 1 || number > 45)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
 }
